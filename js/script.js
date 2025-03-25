87ac1bd46ed4171c3dd66acb7ea71709c7d1478e
andreamaluta <andreamaluta@gmail.com>
2025-03-25 14:54:35 +0100

    [UPD] added the event listener for the mouse click and turn on the lamp

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ const lamp = document.getElementById('off');
 console.log(lamp);
 const button = document.getElementById('btn');
 console.log(button);
+
+
+button.addEventListener('click', function(){
+    lamp.src = '../img/yellow_lamp.png';
+})
